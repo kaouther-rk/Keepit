8c11ad1361e013500d0e25848108e8867f94559a
kepak <reghdikaouther7@gmail.com>
2025-03-11 20:46:47 +0100

    second

---
+++ app/src/main/java/com/example/keepit/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.keepit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.inappmessaging.model.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        TextView welcome = findViewById(R.id.tv_welcome);
+
+
+
+        Intent intent = getIntent();
+        String email =intent.getStringExtra("email");
+
+        welcome.setText("ur email is :" + email);
+
+
+        }
+
+
+  }
